aa13c9cf0894f4f5089b091cb53dbb3fe4108752
Marijn de Mul <106752077+Marijn-de-Mul@users.noreply.github.com>
2024-09-24 14:41:48 +0200

    (Frontend) added main menu

--- DeepDrunkTalk.Frontend/app/routes/_index.tsx
+++ DeepDrunkTalk.Frontend/app/routes/_index.tsx
@@ -1,7 +1,10 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Button } from '@mantine/core';
+import { Button , Divider } from '@mantine/core';
 
 import logo from "../assets/img/logo.png"; 
+
+import "../assets/css/general.css"
+import "../assets/css/mainmenu.css"
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,17 +13,59 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+function viewWidth() {
+  return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+}
+
+function viewHeight() {
+  return Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+}
+
 export default function Index() {
   return (
     <> 
 
-    <div> 
-      <img src={logo}/>
+    <div
+      
+      className="logodiv"
+
+    > 
+      <img 
+    
+        className="logo"
+      
+        src={logo}
+      
+      />
+
     </div>
 
-
-    <Button variant="outline" color="cyan">Button</Button>
+    <div  
     
+      className="menubuttonsdiv"
+    
+    > 
+
+      <Button className="menubutton" variant="filled" color="rgba(0, 0, 0, 1)" size="xl">START GAME</Button>;
+
+      <Button className="menubutton" variant="filled" color="rgba(0, 0, 0, 1)" size="xl">SOBER MODE</Button>;
+
+      <Button className="menubutton" variant="filled" color="rgba(0, 0, 0, 1)" size="xl">SETTINGS</Button>;
+
+    </div>
+
+    <Divider color="black" className="divider"></Divider>
+    
+    <div 
+    
+      className="footerdiv"
+
+    > 
+
+      <p className="footertext"> DeepDrunkTalks - 2024 © </p>
+
+    </div>
+
     </>
   );
 }

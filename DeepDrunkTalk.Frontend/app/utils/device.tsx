bc86333196eb9125deba1e7844afd650e3b070e6
Marijn de Mul <106752077+Marijn-de-Mul@users.noreply.github.com>
2024-12-14 20:51:16 +0100

    Mobile device check optimization

--- DeepDrunkTalk.Frontend/app/utils/device.tsx
+++ DeepDrunkTalk.Frontend/app/utils/device.tsx
@@ -1,3 +1,17 @@
 export const isMobileDevice = () => {
-  return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone|webOS|Mobile/i.test(navigator.userAgent);
+  const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
+
+  if (/iPhone|iPad|iPod/.test(userAgent) && !(window as any).MSStream) {
+    return true;
+  }
+
+  if (/Android/.test(userAgent)) {
+    return true;
+  }
+
+  if (/Mobi|BlackBerry|IEMobile|Opera Mini|Windows Phone|webOS/.test(userAgent)) {
+    return true;
+  }
+
+  return false;
 };

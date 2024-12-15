6a2627284ccc089b2eb3265ab1fab1a5fae9de49
Marijn de Mul <106752077+Marijn-de-Mul@users.noreply.github.com>
2024-12-15 18:47:59 +0100

    E2E-tests update

--- DeepDrunkTalk.Frontend/playwright.config.ts
+++ DeepDrunkTalk.Frontend/playwright.config.ts
@@ -1,6 +1,26 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  testDir: './tests',
+  timeout: 60000,
+  retries: 2,
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  
+  use: {
+    baseURL: (() => {
+      switch (process.env.NODE_ENV) {
+        case 'production':
+          return 'https://ddt.fhict.marijndemul.nl';
+        case 'test':
+          return 'https://ddt.staging.fhict.marijndemul.nl';
+        default:
+          return 'http://localhost:3000';
+      }
+    })(),
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+  },
   projects: [
     {
       name: 'Mobile Safari',
